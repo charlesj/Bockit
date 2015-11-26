42e8147ee35f5c8ee712d3f101cbef6714aa9f15
Josh Charles <josh.charles@gmail.com>
2015-11-26 13:31:42 -0800

    added simple interface for type conversion

--- src/Pancakes/ITypeConverter.cs
+++ src/Pancakes/ITypeConverter.cs
@@ -1,7 +1,11 @@
+using System;
+
 namespace Pancakes
 {
     public interface ITypeConverter
     {
+        object Convert(object value, Type targetType);
         
+        TTargetType Convert<TTargetType>(object value);
     }
 }
