82c69656d457f2a4a67a5e9cb83b97a919d25e5e
Josh Charles <josh.charles@gmail.com>
2015-12-02 20:06:18 -0800

    BaseUnitTest.Build looking green

--- src/Pancakes.TestUtilities/BaseUnitTest.cs
+++ src/Pancakes.TestUtilities/BaseUnitTest.cs
@@ -1,7 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Pancakes.TestUtilities
 {
 	public class BaseUnitTest<TSystemUnderTest>
 	{
-		
+		public TSystemUnderTest Build()
+		{
+			var mockRegistry = new NSubstituteMockRegistry();
+			var constructorParams = new List<object>();
+			var constructors = (typeof(TSystemUnderTest)).GetConstructors();
+			if(constructors.Count() > 1)
+				throw new InvalidOperationException("Cannot Test Types with multiple constructors - Code Smell");
+			
+			var constructor = constructors[0];
+			var parameters = constructor.GetParameters();
+			foreach (var parameter in parameters)
+			{
+				var parametType = parameter.ParameterType;
+				if(!parametType.IsAbstract && !parametType.IsInterface)
+					throw new InvalidOperationException("Use more appropriate types (interface or abstract) for your dependencies");
+					
+				constructorParams.Add(mockRegistry.Get(parameter.ParameterType));
+			}
+			
+			var instance = constructor.Invoke(constructorParams.ToArray()); 
+			return (TSystemUnderTest)instance;
+		}
 	}
 }
--- src/Pancakes.Tests/TestUtiltities/BaseUnitTestTests.cs
+++ src/Pancakes.Tests/TestUtiltities/BaseUnitTestTests.cs
@@ -1,7 +1,94 @@
+using System;
+using Pancakes.TestUtilities;
+using Xunit;
+
 namespace Pancakes.Tests.TestUtilities
 {
 	public class BaseUnitTest
 	{
+		public class Build
+		{
+			[Fact]
+			public void TypeWithMultipleConstructors_ThrowsException()
+			{
+				var sut = new BaseUnitTest<TypeWithMultipleConstructors>();
+				Assert.Throws<InvalidOperationException>(() => sut.Build());
+			}
+			
+			[Fact]
+			public void CanBuildType_WithNoDependencies()
+			{
+				var sut = new BaseUnitTest<TestableTypeNoDependencies>();
+				var built = sut.Build();
+			}
+			
+			[Fact]
+			public void CanBuildType_WithInterfaceDependencies()
+			{
+				var sut = new BaseUnitTest<TypeWithInterfaceDependencies>();
+				var built = sut.Build();
+			}
+			
+			[Fact]
+			public void CanBuildType_WithAbstractDependencies()
+			{
+				var sut = new BaseUnitTest<TypeWithAbstractDependcies>();
+				var buildt = sut.Build();
+			}
+			
+			[Fact]
+			public void ThrowsException_WhenBuildingClassWithInappropriateDependencies()
+			{
+				var sut = new BaseUnitTest<TypeWithInappropriateDependencies>();
+				Assert.Throws<InvalidOperationException>(() => sut.Build());
+			}
+		}
 		
+		public class TypeWithInappropriateDependencies
+		{
+			public TypeWithInappropriateDependencies(string inappropriate)
+			{}
+		}
+		
+		public class TypeWithInterfaceDependencies
+		{
+			public TypeWithInterfaceDependencies(IFooService fooService, IBarService barService)
+			{
+			}
+		}
+		
+		public class TypeWithAbstractDependcies
+		{
+			public TypeWithAbstractDependcies(TestAbstractClass tac)
+			{}
+		}
+		
+		public abstract class TestAbstractClass
+		{}
+		
+		public class TypeWithMultipleConstructors
+		{
+			public TypeWithMultipleConstructors()
+			{
+				
+			}
+			
+			public TypeWithMultipleConstructors(IFooService fooService)
+			{
+				
+			}
+		}
+		
+		public class TestableTypeNoDependencies
+		{	
+		}
+		
+		public interface IFooService
+		{
+		}
+		
+		public interface IBarService
+		{
+		}
 	}	
 }
